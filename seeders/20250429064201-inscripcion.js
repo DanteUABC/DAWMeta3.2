af9f8fe80107d9b177fe9bdcc75206d246ce817f
DanteUABC <125496940+DanteUABC@users.noreply.github.com>
2025-05-19 12:40:59 -0700

    Update 20250429064201-inscripcion.js

--- seeders/20250429064201-inscripcion.js
+++ seeders/20250429064201-inscripcion.js
@@ -4,8 +4,8 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Inscripciones', [
-      { estudianteId: 1, asignaturaId: 1, semestre: 1, calificacion: 8.8, createdAt: new Date(), updatedAt: new Date() },
-      { estudianteId: 1, asignaturaId: 2, semestre: 1, calificacion: 7.3, createdAt: new Date(), updatedAt: new Date() }
+      { id: 1, estudianteId: 1, asignaturaId: 1, semestre: 1, calificacion: 8.8, createdAt: new Date(), updatedAt: new Date() },
+      { id: 2, estudianteId: 1, asignaturaId: 2, semestre: 1, calificacion: 7.3, createdAt: new Date(), updatedAt: new Date() }
     ]);
   },
 
